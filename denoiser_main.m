8254d78778a48d668d7c8bba5406a696c8be3010
Gaurav Somani <gsomani707@gmail.com>
2018-04-20 15:59:52 +0530

    Add Comments

--- denoiser_main.m
+++ denoiser_main.m
@@ -1,13 +1,18 @@
+% load image %
 I = double(imread('test_images/girlface.bmp'));
 I = I./ 255;
 v = var(I(:));
 disp(v);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Add gaussian noise %
 %u = imnoise(I, 'gaussian', sqrt(v));
 u = I + randn(size(I)) .* (5*v);
 u(u < 0) = 0;
 u(u > 1) = 1;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Apply Soft Wavelet Thresholding on the noised image %
 x = u;
 [thr,sorh,keepapp] = ddencmp('den','wv',x);
 wv = 'sym4';
@@ -15,6 +20,7 @@ level = 3;
 sorh = 's';
 b = wdencmp('gbl',x,wv,level,thr,sorh,keepapp);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %subplot(2,2,1), imshow(I);
 %subplot(2,2,2), imshow(u);
 %subplot(2,2,3), imshow(b);
@@ -23,11 +29,16 @@ mkdir('Projects/Image_Processing_Denoising/denoised_soft');
 imwrite(u, 'Projects/Image_Processing_Denoising/noised_images/girlface_noised.bmp');
 imwrite(b, 'Projects/Image_Processing_Denoising/denoised_soft/girlface_soft.bmp');
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Set Parameters for diffusion %
 sigma = v;
 k = 60;
 lambda1 = 40;
 lambda2 = 1;
 tau = 0.0001;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Apply Gaussian Filtering and Parona-Malik(PM) Anisotropic Diffusion %
 bsigma = imgaussfilt(b,sigma);
 pmb = PMdiffuse(bsigma,k, lambda2);
 
@@ -43,3 +54,4 @@ end
 %subplot(2,2,4), imshow(u);
 mkdir('Projects/Image_Processing_Denoising/denoised_diffused');
 imwrite(u, 'Projects/Image_Processing_Denoising/denoised_diffused/girlface_diffused.bmp');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
